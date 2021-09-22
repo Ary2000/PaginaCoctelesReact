c3c6bd4bf50fc25166a2ae2cf071569d7db55b24
Ary2000 <aryelduranballestero@gmail.com>
2021-09-21 21:32:05 -0600

    interface

--- react-tasks-app/src/componentsLogin/Profile.tsx
+++ react-tasks-app/src/componentsLogin/Profile.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import JSONPretty from 'react-json-pretty';
 
+interface Usuario{
+  nickname: string
+}
+
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0<Usuario>();
+  alert(user?.nickname);
   if(isAuthenticated== undefined){
     return (<div>hola</div>);
   }
